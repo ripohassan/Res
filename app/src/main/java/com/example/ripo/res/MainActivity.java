e9675c5efbe7f1f9f2fbcdb13c63892a7f2b7015
ripo <ripohassan5@gmail.com>
2018-01-20 11:58:05 +0600

    Button click show massage

--- app/src/main/java/com/example/ripo/res/MainActivity.java
+++ app/src/main/java/com/example/ripo/res/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.ripo.res;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =(Button)findViewById(R.id.roxa);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hello World",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
